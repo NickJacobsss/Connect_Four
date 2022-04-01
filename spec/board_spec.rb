4104b0f5dee40fc341ed2484e63e101c130b26fa
Thiago Silveira <thiago1992@yahoo.com>
2022-03-31 20:21:44 -0600

    Feat: Player Column Selection

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,15 @@
 require './lib/board'
 
 RSpec.describe Board do
-  it 'exists' do
-    
+
+  it "exists" do
+    board = Board.new
+    expect(board).to be_an_instance_of(Board)
+  end
+
+  it "tests make_board" do
+    board = Board.new
+    board.make_board
+
   end
 end
