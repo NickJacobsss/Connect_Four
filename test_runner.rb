f8a8872959a4bfd5e926ec1257a86001edc50399
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-01 18:06:34 -0500

    Fix: Commented out pseudocode

--- test_runner.rb
+++ test_runner.rb
@@ -4,20 +4,22 @@ board = Board.new
 board.intro
 
 
-# player1_turn
-#
-# puts "select a row a-g"
-# if user.input == a
-#   player_selection_a
-#
-# def player_selection_b
-#   if a1 == ". "
-#     a1 = "x"
-#   elsif a2 == ". "
-#     a2 = "x"
-#   else
-#     puts "try again, column A is full"
-#     check_winner
-#       player1_turn
-#
-#       def check_winner
+
+  # if user_input == a
+  #   player_selection_a
+  # elsif user_input == b
+  #   player_selection_b
+  #
+  #
+  # def player_selection_a
+  #   if @board[:a1] == ". "
+  #     @board[:a1] = "x"
+  #   elsif @board[:a2] == ". "
+  #     @board[:a2] = "x"
+  #   else
+  #     puts "try again, column A is full"
+  #     check_winner
+  #     start_turn
+  #
+  #    def computer_turn
+  #     options = [1,2,3,4,5,6,7](argument to randomize)
