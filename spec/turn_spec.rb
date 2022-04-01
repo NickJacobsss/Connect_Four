4a2a8e270a9c3c52a4627f1b0b85fa5f9f260f15
Thiago Silveira <thiago1992@yahoo.com>
2022-03-31 20:22:20 -0600

    Test: Player Column Selection

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,22 @@
 require './lib/board'
 require './lib/turn'
+require 'pry'
 
 RSpec.describe Turn do
 
-  it "exists" do
-    turn = Turn.new
+  xit "exists" do
+    board = Board.new.board
+    turn = Turn.new(board)
     expect(turn).to be_an_instance_of(Turn)
+  end
+
+  it "test player column selection" do
+    board = Board.new.board
+    turn = Turn.new(board)
+    # binding.pry
+    @player_selection = "A"
+    # turn.player_column_selection
+    turn.column_a_selected
+    expect(board[:A1]).to eq("X ")
   end
 end
