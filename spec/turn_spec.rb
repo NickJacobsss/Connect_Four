964c40faeac88389951625e6ccd6fceb79c706cc
Thiago Silveira <thiago1992@yahoo.com>
2022-04-01 14:53:24 -0600

    Test: Piece Placement

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -11,12 +11,17 @@ RSpec.describe Turn do
   end
 
   it "test player column selection" do
-    board = Board.new.board
-    turn = Turn.new(board)
+    board = Board.new
+    turn = Turn.new
+    # turn.start_turn
+    turn.piece_placement("A")
+
+    expect(turn.board.board[:A1]).to eq("X ")
+
     # binding.pry
-    @player_selection = "A"
-    # turn.player_column_selection
-    turn.column_a_selected
-    expect(board[:A1]).to eq("X ")
+    # @player_selection = "A"
+    # turn.piece_placement
+    # turn.column_a_selected
+    # expect(board[:A1]).to eq("X ")
   end
 end
