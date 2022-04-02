1a361f6e4439734aaec0d2a2a05363b3a31b2bf8
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-02 16:03:01 -0500

    Initial Commit

---
+++ spec/game_spec.rb
@@ -0,0 +1,14 @@
+require './lib/board'
+require './lib/player'
+require './lib/game'
+require './lib/computer'
+require 'pry'
+
+RSpec.describe Game do
+
+  it "exists" do
+    game = Game.new
+    expect(game).to be_an_instance_of(Game)
+  end
+  
+end
