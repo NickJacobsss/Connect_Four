74550f6123486bb64c8dc73d78712d1efa0ab4e9
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-02 16:03:49 -0500

    Refactor: Removed excess comments

--- test_runner.rb
+++ test_runner.rb
@@ -2,24 +2,3 @@ require './lib/game'
 
 game = Game.new
 game.intro
-
-
-
-  # if user_input == a
-  #   player_selection_a
-  # elsif user_input == b
-  #   player_selection_b
-  #
-  #
-  # def player_selection_a
-  #   if @board[:a1] == ". "
-  #     @board[:a1] = "x"
-  #   elsif @board[:a2] == ". "
-  #     @board[:a2] = "x"
-  #   else
-  #     puts "try again, column A is full"
-  #     check_winner
-  #     start_turn
-  #
-  #    def computer_turn
-  #     options = [1,2,3,4,5,6,7](argument to randomize)
