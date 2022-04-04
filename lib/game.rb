d928b5d46d6cf49e71d7269511bf3a07225f078b
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-04 12:08:16 -0500

    Feat: added loop for player & computer turns

--- lib/game.rb
+++ lib/game.rb
@@ -3,13 +3,12 @@ require './lib/player'
 require './lib/computer'
 
 class Game
-  attr_reader :board
+  attr_reader :board, :player, :computer
 
   def initialize
     @board = Board.new
-    @player = Player.new
-    @computer = Computer.new
-    @turn = "player"
+    @player = Player.new(@board)
+    @computer = Computer.new(@board)
   end
 
   def intro
@@ -20,6 +19,19 @@ class Game
       puts "------------------------"
       gets.chomp
       @board.make_board
+      turn_loop
       # start_turn
+  end
+
+  def turn_loop
+    loop {
+    @player.player_turn
+    @computer.computer_turn
+    @board.make_board
+    if !@board.board.values.include?(". ")
+      puts "The game is a draw"
+      break
+    end 
+  }
   end
 end
