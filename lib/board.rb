47c73c2639193864cf30759632a2f18543207e59
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-05 14:05:15 -0500

    Feat: added refresh_board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+
 class Board
 attr_reader :board
 
@@ -15,6 +16,7 @@ attr_reader :board
   def make_board
     rows = ["6","5","4","3","2","1"]
     puts "A B C D E F G"
+    puts "-------------"
     rows.each do |row|
       current_row = {}
       @board.each do |key, val|
@@ -24,5 +26,18 @@ attr_reader :board
       end
       puts current_row.values.join
     end
+  end
+
+  def refresh_board
+    @board = {
+      A6: ". ", B6: ". ", C6: ". ", D6: ". ", E6: ". ", F6: ". ", G6: ". ",
+      A5: ". ", B5: ". ", C5: ". ", D5: ". ", E5: ". ", F5: ". ", G5: ". ",
+      A4: ". ", B4: ". ", C4: ". ", D4: ". ", E4: ". ", F4: ". ", G4: ". ",
+      A3: ". ", B3: ". ", C3: ". ", D3: ". ", E3: ". ", F3: ". ", G3: ". ",
+      A2: ". ", B2: ". ", C2: ". ", D2: ". ", E2: ". ", F2: ". ", G2: ". ",
+      A1: ". ", B1: ". ", C1: ". ", D1: ". ", E1: ". ", F1: ". ", G1: ". "
+    }
+    puts "Press any key to continue"
+    gets.chomp
   end
 end
